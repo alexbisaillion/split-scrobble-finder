bb1ff497a2120bfe3bc71eb8b60d319131dd6df3
Alex Bisaillion <a.bisaillion@gmail.com>
2019-12-29 16:24:23 -0500

    Include URL for each track

--- split.js
+++ split.js
@@ -3,6 +3,7 @@ const stringSimilarity = require('string-similarity');
 const credentials = require('./credentials');
 const isDuplicateTrack = require('./trackRules');
 const fs = require('fs');
+const user = 'watzpoppiin';
 
 function getOptions(params) {
   return {
@@ -24,7 +25,7 @@ function getURL(params) {
 }
 
 function getNumTracks() {
-  request(getOptions({method: 'user.getTopTracks', user: 'watzpoppiin', limit: 1}), function (error, response, body) {
+  request(getOptions({method: 'user.getTopTracks', user: user, limit: 1}), function (error, response, body) {
     if (error) {
       console.log('Error: ' + error);
     } else {
@@ -34,7 +35,7 @@ function getNumTracks() {
 }
 
 function getTracks(numTracks, pageNum, tracks) {
-  request(getOptions({method: 'user.getTopTracks', user: 'watzpoppiin', limit: 1000, page: pageNum}), function (error, response, body) {
+  request(getOptions({method: 'user.getTopTracks', user: user, limit: 1000, page: pageNum}), function (error, response, body) {
     if (error) {
       console.log('Error: ' + error);
     } else if (!JSON.parse(body).toptracks) {
@@ -54,33 +55,37 @@ function partitionTracks(tracks) {
   let partitioned = {};
   for (let i = 0; i < tracks.length; i++) {
     if (!partitioned[tracks[i].artist.name]) {
-      partitioned[tracks[i].artist.name] = [tracks[i].name]
+      partitioned[tracks[i].artist.name] = [{name: tracks[i].name, url: getTrackInUserLibrary(tracks[i].url, user)}]
     } else {
-      partitioned[tracks[i].artist.name].push(tracks[i].name);
+      partitioned[tracks[i].artist.name].push({name: tracks[i].name, url: getTrackInUserLibrary(tracks[i].url, user)});
     }
   }
+  
   let matched = [];
   for (let artist of Object.keys(partitioned)) {
-    partitioned[artist].sort();
+    partitioned[artist].sort((a, b) => (a.name > b.name) ? 1 : -1);
     for (let i = 0; i < partitioned[artist].length - 1; i++) {
-      let track1 = partitioned[artist][i];
-      let track2 = partitioned[artist][i + 1];
+      let track1 = partitioned[artist][i].name;
+      let track2 = partitioned[artist][i + 1].name;
       if (isDuplicateTrack(track1, track2)) {
         // If not matched, should continue comparing
-        matched.push(partitioned[artist][i] + ', ' + partitioned[artist][i + 1]);
-        console.log(partitioned[artist][i] + ', ' + partitioned[artist][i + 1]);
-        console.log(stringSimilarity.compareTwoStrings(partitioned[artist][i], partitioned[artist][i + 1]));
+        matched.push({track1: track1, track2: track2});
       }
     }
   }
 
-  /*
-  fs.writeFile("test7.txt", JSON.stringify(matched), function(err) {
+
+  
+  fs.writeFile("test8.txt", JSON.stringify(matched), function(err) {
     if (err) {
       console.log(err);
     }
   });
-  */
+  
+}
+
+function getTrackInUserLibrary(trackURL, user) {
+  return trackURL.replace('/music/', `/user/${user}/library/music/`);
 }
 
 getNumTracks();
