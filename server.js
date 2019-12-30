c976590a953e5e98a80e3003fb77d00a77a4e718
Alex Bisaillion <a.bisaillion@gmail.com>
2019-12-30 11:26:20 -0500

    Move business logic to server

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 const credentials = require('./credentials');
+const isDuplicateTrack = require('./trackRules');
+
 const app = express();
+const { PerformanceObserver, performance } = require('perf_hooks');
+let start;
+let end;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/tracks', getNumTracks, getTracks, (req, res, next) => {
-  if (req.query.user) {
-    next();
-  } else {
-    res.status(400).send("Bad request.");
-  }
-});
+app.get('/tracks', validateReq, getNumTracks, getTracks, partitionTracks, getDuplicates);
 
 function getOptions(params) {
   return {
@@ -32,6 +31,19 @@ function getURL(params) {
   url.searchParams.append('format', 'json');
   url.searchParams.append('api_key', credentials.API_KEY);
   return url;
+}
+
+function getTrackInUserLibrary(trackURL, user) {
+  return trackURL.replace('/music/', `/user/${user}/library/music/`);
+}
+
+function validateReq(req, res, next) {
+  if (req.query.user) {
+    start = performance.now();
+    next();
+  } else {
+    res.status(400).send("Bad request.");
+  }
 }
 
 function getNumTracks(req, res, next) {
@@ -54,10 +66,10 @@ function getTracks(req, res, next) {
     request(getOptions({method: 'user.getTopTracks', user: req.query.user, limit: 1000, page: res.locals.pageNum}), function (error, response, body) {
       if (error) {
         console.log('Error: ' + error);
-        res.status(500).send("Internal error.")
+        res.status(500).send("Internal error.");
       } else if (!JSON.parse(body).toptracks) {
         console.log('Last.fm API error');
-        res.status(500).send("Internal error.")
+        res.status(500).send("Internal error.");
       } else {
         res.locals.tracks.push(...JSON.parse(body).toptracks.track);
         if (res.locals.numTracks - 1000 > 0) {
@@ -65,11 +77,42 @@ function getTracks(req, res, next) {
           res.locals.pageNum = res.locals.pageNum + 1;
           getPage();
         } else {
-          res.json(res.locals.tracks);
+          next();
         }
       }
     });
   }
+}
+
+function partitionTracks(req, res, next) {
+  let partitioned = {};
+  let tracks = res.locals.tracks;
+  for (let i = 0; i < tracks.length; i++) {
+    if (!partitioned[tracks[i].artist.name]) {
+      partitioned[tracks[i].artist.name] = [tracks[i].name]
+    } else {
+      partitioned[tracks[i].artist.name].push(tracks[i].name);
+    }
+  }
+  res.locals.partitioned = partitioned;
+  next()
+}
+
+function getDuplicates(req, res, next) {
+  let matched = [];
+  let partitioned = res.locals.partitioned;
+  for (let artist of Object.keys(partitioned)) {
+    partitioned[artist].sort();
+    for (let i = 0; i < partitioned[artist].length - 1; i++) {
+      if (isDuplicateTrack(partitioned[artist][i], partitioned[artist][i + 1])) {
+        // TODO: If a match is found, compare against the next track to find multiple duplicates
+        matched.push({artist: artist, track1: partitioned[artist][i], track2: partitioned[artist][i + 1]});
+      }
+    }
+  }
+  end = performance.now();
+  console.log(end - start);
+  res.json(matched);
 }
 
 const port = process.env.PORT || 3001;
