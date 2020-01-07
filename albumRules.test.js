ab89e514bb948ac55e28e94e196742018f5adb49
Alex Bisaillion <a.bisaillion@gmail.com>
2020-01-07 11:01:48 -0500

    Update test suites to use rules

--- albumRules.test.js
+++ albumRules.test.js
@@ -2,129 +2,129 @@ const rules = require('./rules');
 
 describe('Check if the isDuplicateAlbum method successfully detects duplicates', () => {
   test('Check if a deluxe album is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Take Care', 'Take Care (Deluxe)')).toBe(true);
+    expect(rules.isDuplicateAlbum('Take Care', 'Take Care (Deluxe)', true)).toBe(true);
   });
   test('Check if an extended album is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Pure Heroine (Extended)', 'Pure Heroine')).toBe(true);
+    expect(rules.isDuplicateAlbum('Pure Heroine (Extended)', 'Pure Heroine', true)).toBe(true);
   });
   test('Check if a single with excess feature tag is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('One Out Of Two', 'One Out Of Two (feat. Irfane)')).toBe(true);
+    expect(rules.isDuplicateAlbum('One Out Of Two', 'One Out Of Two (feat. Irfane)', true)).toBe(true);
   });
   test('Check if a single with features listed in different order is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Watch (feat. Kanye West & Lil Uzi Vert)', 'Watch (feat. Lil Uzi Vert & Kanye West)')).toBe(true);
+    expect(rules.isDuplicateAlbum('Watch (feat. Kanye West & Lil Uzi Vert)', 'Watch (feat. Lil Uzi Vert & Kanye West)', true)).toBe(true);
   });
   test('Check if a single with \'single\' listed in the title is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('This Is America', 'This Is America - Single')).toBe(true);
+    expect(rules.isDuplicateAlbum('This Is America', 'This Is America - Single', true)).toBe(true);
   });
   test('Check if an EP with \'EP\' listed in the title is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Rogue Waves', 'Rogue Waves - EP')).toBe(true);
+    expect(rules.isDuplicateAlbum('Rogue Waves', 'Rogue Waves - EP', true)).toBe(true);
   });
   test('Check if an album with extra nonalphanumeric characters is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Born to Die (The Paradise Edition)', 'Born to Die - The Paradise Edition')).toBe(true);
+    expect(rules.isDuplicateAlbum('Born to Die (The Paradise Edition)', 'Born to Die - The Paradise Edition', true)).toBe(true);
   });
   test('Check if a single with an excess feature tag listed as \'single\' is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Drug Dealers Anonymous', 'Drug Dealers Anonymous (feat. JAY Z) - Single')).toBe(true);
+    expect(rules.isDuplicateAlbum('Drug Dealers Anonymous', 'Drug Dealers Anonymous (feat. JAY Z) - Single', true)).toBe(true);
   });
   test('Carly Slay Jepsen', () => {
-    expect(rules.isDuplicateAlbum('E\u00b7MO\u00b7TION Side B', 'EMOTION SIDE B +')).toBe(true);
+    expect(rules.isDuplicateAlbum('E\u00b7MO\u00b7TION Side B', 'EMOTION SIDE B +', true)).toBe(true);
   });
   test('last dinos', () => {
-    expect(rules.isDuplicateAlbum('In A Million Years', 'In A Million Years (Tour Edition)')).toBe(true);
+    expect(rules.isDuplicateAlbum('In A Million Years', 'In A Million Years (Tour Edition)', true)).toBe(true);
   });
   test('Check if album with extra whitespace is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Funk Wav Bounces Vol. 1', 'Funk Wav Bounces Vol.1')).toBe(true);
+    expect(rules.isDuplicateAlbum('Funk Wav Bounces Vol. 1', 'Funk Wav Bounces Vol.1', true)).toBe(true);
   });
   test('Check if the explicit version of an album is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Man On The Moon II: The Legend Of Mr. Rager (Explicit Version)', 'Man On The Moon, Vol. II: The Legend Of Mr. Rager')).toBe(true);
+    expect(rules.isDuplicateAlbum('Man On The Moon II: The Legend Of Mr. Rager (Explicit Version)', 'Man On The Moon, Vol. II: The Legend Of Mr. Rager', true)).toBe(true);
   });
   test('Check if an album missing \'vol\' is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Man On The Moon, Vol. II: The Legend Of Mr. Rager', 'Man on the Moon II: The Legend of Mr. Rager')).toBe(true);
+    expect(rules.isDuplicateAlbum('Man On The Moon, Vol. II: The Legend Of Mr. Rager', 'Man on the Moon II: The Legend of Mr. Rager', true)).toBe(true);
   });
   test('Check if a deluxe version of an album is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Sun Structures', 'Sun Structures (Deluxe Version)')).toBe(true);
+    expect(rules.isDuplicateAlbum('Sun Structures', 'Sun Structures (Deluxe Version)', true)).toBe(true);
   });
   test('Check if an album using an extra ellipsis is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Magna Carta Holy Grail', 'Magna Carta... Holy Grail')).toBe(true);
+    expect(rules.isDuplicateAlbum('Magna Carta Holy Grail', 'Magna Carta... Holy Grail', true)).toBe(true);
   });
   test('Check if an album name with an extra article is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Vol. 3: Life and Times of S. Carter', 'Vol. 3: The Life and Times of S. Carter')).toBe(true);
+    expect(rules.isDuplicateAlbum('Vol. 3: Life and Times of S. Carter', 'Vol. 3: The Life and Times of S. Carter', true)).toBe(true);
   });
   test('Check if a deluxe edition of an album is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Everything You\'ve Come To Expect (Deluxe Edition)', 'Everything You\u2019ve Come To Expect')).toBe(true);
+    expect(rules.isDuplicateAlbum('Everything You\'ve Come To Expect (Deluxe Edition)', 'Everything You\u2019ve Come To Expect', true)).toBe(true);
   });
   test('Check if an abbreviation without punctuation is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Sept 5th', 'Sept. 5th')).toBe(true);
+    expect(rules.isDuplicateAlbum('Sept 5th', 'Sept. 5th', true)).toBe(true);
   });
   test('Check if an album listed as bonus track is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('The Bird Of Music', 'The Bird Of Music (Bonus Track)')).toBe(true);
+    expect(rules.isDuplicateAlbum('The Bird Of Music', 'The Bird Of Music (Bonus Track)', true)).toBe(true);
   });
   test('Check if the expanded and deluxe edition of an album are duplicates', () => {
-    expect(rules.isDuplicateAlbum('Hip Hop Is Dead (Deluxe Edition)', 'Hip Hop Is Dead (Expanded Edition)')).toBe(true);
+    expect(rules.isDuplicateAlbum('Hip Hop Is Dead (Deluxe Edition)', 'Hip Hop Is Dead (Expanded Edition)', true)).toBe(true);
   });
   test('Check if a \/ with different spacing is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Speakerboxxx \/ The Love Below', 'Speakerboxxx\/The Love Below')).toBe(true);
+    expect(rules.isDuplicateAlbum('Speakerboxxx \/ The Love Below', 'Speakerboxxx\/The Love Below', true)).toBe(true);
   });
   test('Check if a special edition of an album is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('The Bones of What You Believe', 'The Bones of What You Believe (Special Edition)')).toBe(true);
+    expect(rules.isDuplicateAlbum('The Bones of What You Believe', 'The Bones of What You Believe (Special Edition)', true)).toBe(true);
   });
   test('Check if an expanded edition marked in square brackets is a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Enter The Wu-Tang (36 Chambers) [Expanded Edition]', 'Enter the Wu-Tang (36 Chambers)')).toBe(true);
+    expect(rules.isDuplicateAlbum('Enter The Wu-Tang (36 Chambers) [Expanded Edition]', 'Enter the Wu-Tang (36 Chambers)', true)).toBe(true);
   });
 });
 
 describe('Check if the isDuplicateAlbum method successfully detects non duplicates', () => {
   test('Check if an album of instrumentals is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Cherry Bomb', 'Cherry Bomb + Instrumentals')).toBe(false);
+    expect(rules.isDuplicateAlbum('Cherry Bomb', 'Cherry Bomb + Instrumentals', true)).toBe(false);
   });
   test('Check if the b sides for two different albums are not duplicates', () => {
-    expect(rules.isDuplicateAlbum('Currents B-Sides & Remixes', 'InnerSpeaker B-Sides & Remixes')).toBe(false);
+    expect(rules.isDuplicateAlbum('Currents B-Sides & Remixes', 'InnerSpeaker B-Sides & Remixes', true)).toBe(false);
   });
   test('Check if the solo version of a single is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Biking', 'Biking (Solo)')).toBe(false);
+    expect(rules.isDuplicateAlbum('Biking', 'Biking (Solo)', true)).toBe(false);
   });
   test('Check if the b side of an album is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('E\u00b7MO\u00b7TION', 'E\u00b7MO\u00b7TION Side B')).toBe(false);
+    expect(rules.isDuplicateAlbum('E\u00b7MO\u00b7TION', 'E\u00b7MO\u00b7TION Side B', true)).toBe(false);
   });
   test('Check if a remix album is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Human After All', 'Human After All (Remixes)')).toBe(false);
+    expect(rules.isDuplicateAlbum('Human After All', 'Human After All (Remixes)', true)).toBe(false);
   });
   test('Check if different remixes of a single are not duplicates', () => {
-    expect(rules.isDuplicateAlbum('Don\'t Leave Me Lonely (Claptone Remix)', 'Don\'t Leave Me Lonely (Purple Disco Machine Remix)')).toBe(false);
+    expect(rules.isDuplicateAlbum('Don\'t Leave Me Lonely (Claptone Remix)', 'Don\'t Leave Me Lonely (Purple Disco Machine Remix)', true)).toBe(false);
   });
   test('Check if a remix of a single is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Late Night Feelings', 'Late Night Feelings (Channel Tres Remix)')).toBe(false);
+    expect(rules.isDuplicateAlbum('Late Night Feelings', 'Late Night Feelings (Channel Tres Remix)', true)).toBe(false);
   });
   test('Check if the sequel to an album using numeric characters is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Luv Is Rage', 'Luv Is Rage 2')).toBe(false);
+    expect(rules.isDuplicateAlbum('Luv Is Rage', 'Luv Is Rage 2', true)).toBe(false);
   });
   test('Check if the sequel to an album using roman numerals is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Culture', 'Culture II')).toBe(false);
+    expect(rules.isDuplicateAlbum('Culture', 'Culture II', true)).toBe(false);
   });
   test('Check if the sequel to an album using written words is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('PARTYNEXTDOOR', 'PARTYNEXTDOOR TWO')).toBe(false);
+    expect(rules.isDuplicateAlbum('PARTYNEXTDOOR', 'PARTYNEXTDOOR TWO', true)).toBe(false);
   });
   test('Check if two different volumes of an album using roman numerals are not duplicates', () => {
-    expect(rules.isDuplicateAlbum('Superclean, Vol. I', 'Superclean, Vol. II')).toBe(false);
+    expect(rules.isDuplicateAlbum('Superclean, Vol. I', 'Superclean, Vol. II', true)).toBe(false);
   });
   test('Check if an edit of a single is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Better Now', 'Better Now (Edit)')).toBe(false);
+    expect(rules.isDuplicateAlbum('Better Now', 'Better Now (Edit)', true)).toBe(false);
   });
   test('Check if two soundtracks by the same artist are not duplicates', () => {
-    expect(rules.isDuplicateAlbum('Blade Runner 2049 (Original Motion Picture Soundtrack)', 'The Dark Knight (Collectors Edition) [Original Motion Picture Soundtrack]')).toBe(false);
+    expect(rules.isDuplicateAlbum('Blade Runner 2049 (Original Motion Picture Soundtrack)', 'The Dark Knight (Collectors Edition) [Original Motion Picture Soundtrack]', true)).toBe(false);
   });
   test('Check if single remixes by two different artists are not duplicates', () => {
-    expect(rules.isDuplicateAlbum('Lightenup (Alex Metric Remix)', 'Lightenup (Breakbot Remix)')).toBe(false);
+    expect(rules.isDuplicateAlbum('Lightenup (Alex Metric Remix)', 'Lightenup (Breakbot Remix)', true)).toBe(false);
   });
   test('Check if a remix EP is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('For All We Know', 'For All We Know - The Remixes - EP')).toBe(false);
+    expect(rules.isDuplicateAlbum('For All We Know', 'For All We Know - The Remixes - EP', true)).toBe(false);
   });
   test('Check if a live album is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('Carrie & Lowell', 'Carrie & Lowell Live')).toBe(false);
+    expect(rules.isDuplicateAlbum('Carrie & Lowell', 'Carrie & Lowell Live', true)).toBe(false);
   });
   test('Check if a live album is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('88GLAM RELOADED', '88GLAM2')).toBe(false);
+    expect(rules.isDuplicateAlbum('88GLAM RELOADED', '88GLAM2', true)).toBe(false);
   });
   test('Check an album with \'ep\' in it without actually being an EP is not a duplicate', () => {
-    expect(rules.isDuplicateAlbum('The Blueprint', 'The Blueprint 2: The Gift & the Curse')).toBe(false);
+    expect(rules.isDuplicateAlbum('The Blueprint', 'The Blueprint 2: The Gift & the Curse', true)).toBe(false);
   });
 });
--- artistRules.test.js
+++ artistRules.test.js
@@ -2,42 +2,42 @@ const rules = require('./rules');
 
 describe('Check if the isDuplicateArtist method successfully detects duplicates', () => {
   test('Check if an artist contained in a \'&\' separated list of two artists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('21 Savage', '21 Savage & Metro Boomin')).toBe(true);
+    expect(rules.isDuplicateArtist('21 Savage', '21 Savage & Metro Boomin', true)).toBe(true);
   });
   test('Check if an artist contained in a \',\' separated list of two artists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('Silk City', 'Silk City, Dua Lipa')).toBe(true);
+    expect(rules.isDuplicateArtist('Silk City', 'Silk City, Dua Lipa', true)).toBe(true);
   });
   test('Check if an artist contained in two \'&\' separated lists of artists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('Calvin Harris & Alesso', 'Calvin Harris & Disciples')).toBe(true);
+    expect(rules.isDuplicateArtist('Calvin Harris & Alesso', 'Calvin Harris & Disciples', true)).toBe(true);
   });
   test('Check if an artist contained in one \'&\' and one \',\' separated lists of artists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('Calvin Harris & Disciples', 'Calvin Harris, Dua Lipa')).toBe(true);
+    expect(rules.isDuplicateArtist('Calvin Harris & Disciples', 'Calvin Harris, Dua Lipa', true)).toBe(true);
   });
   test('Check if an artist contained in a list of 3 artists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('Ellie Goulding', 'Ellie Goulding, Diplo & Swae Lee')).toBe(true);
+    expect(rules.isDuplicateArtist('Ellie Goulding', 'Ellie Goulding, Diplo & Swae Lee', true)).toBe(true);
   });
   test('Check if an artist contained in two large lists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('Kanye West, Big Sean, Pusha T & 2 Chainz', 'Kanye West, Chief Keef, Pusha T, Big Sean & Jadakiss')).toBe(true);
+    expect(rules.isDuplicateArtist('Kanye West, Big Sean, Pusha T & 2 Chainz', 'Kanye West, Chief Keef, Pusha T, Big Sean & Jadakiss', true)).toBe(true);
   });
   test('Check if an artist contained in a list of two artists and a list of three artists is a duplicate', () => {
-    expect(rules.isDuplicateArtist('Lil Baby & Gunna', 'Lil Baby, Gunna & Drake')).toBe(true);
+    expect(rules.isDuplicateArtist('Lil Baby & Gunna', 'Lil Baby, Gunna & Drake', true)).toBe(true);
   });
   test('Potato Salad', () => {
-    expect(rules.isDuplicateArtist('Tyler, the Creator', 'Tyler, The Creator & A$AP Rocky')).toBe(true);
+    expect(rules.isDuplicateArtist('Tyler, the Creator', 'Tyler, The Creator & A$AP Rocky', true)).toBe(true);
   });
 });
 
 describe('Check if the isDuplicateArtist method successfully detects non duplicates', () => {
   test('Check if different artists with one matching the beginning of the other are not duplicates', () => {
-    expect(rules.isDuplicateArtist('America', 'American Football')).toBe(false);
+    expect(rules.isDuplicateArtist('America', 'American Football', true)).toBe(false);
   });
   test('Check if different artists with with the same first name are not duplicates', () => {
-    expect(rules.isDuplicateArtist('Anthony Green', 'Anthony Naples')).toBe(false);
+    expect(rules.isDuplicateArtist('Anthony Green', 'Anthony Naples', true)).toBe(false);
   });
   test('Check if an artist with only first name is not a duplicate of another artist with same first name and a last name', () => {
-    expect(rules.isDuplicateArtist('Arthur', 'Arthur Brown')).toBe(false);
+    expect(rules.isDuplicateArtist('Arthur', 'Arthur Brown', true)).toBe(false);
   });
   test('Check if an artist with only first name is not a duplicate of another artist with same first name and a last name', () => {
-    expect(rules.isDuplicateArtist('Desire', 'Desired')).toBe(false);
+    expect(rules.isDuplicateArtist('Desire', 'Desired', true)).toBe(false);
   });
 });
